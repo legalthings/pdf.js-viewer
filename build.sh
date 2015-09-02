81180874134f029605f4cff577e9503438ce58c2
Arnold Daniels <arnold@jasny.net>
2015-09-02 16:24:43 +0200

    Don't combine pdf.worker.js (loaded externally) Autodetermine worker cmaps and images path in pdf.js

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
-uglifyjs source/web/l10n.js source/build/pdf.js source/build/pdf.worker.js source/web/compatibility.js source/web/debugger.js source/web/viewer.js > pdf.js
+cp source/build/pdf.worker.js source/web/cmaps/ source/web/images/ source/web/locale/ . -a
 
-cp source/web/cmaps/ source/web/images/ source/web/locale/ . -a
+cat source/web/l10n.js source/build/pdf.js source/build/pdf.worker.js source/web/compatibility.js source/web/debugger.js source/web/viewer.js > pdf.js
 
 uglifycss source/web/viewer.css > viewer.css
 node grunt/css-prefix.js viewer.css viewer.css pdfjs
