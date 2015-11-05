fcb4e568f1ae35164d6f0c09e5eafd16cd1ede3f
Arnold Daniels <arnold@jasny.net>
2015-11-04 21:22:19 -0400

    Added patch file with custom changes

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 cp source/build/pdf.worker.js source/web/cmaps/ source/web/images/ source/web/locale/ . -a
 
 cat source/web/l10n.js source/build/pdf.js source/build/pdf.worker.js source/web/compatibility.js source/web/debugger.js source/web/viewer.js > pdf.js
+patch pdf.js < pdf.js.patch
 
 uglifycss source/web/viewer.css > viewer.css
 node grunt/css-prefix.js viewer.css viewer.css pdfjs
