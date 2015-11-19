580cd810356b34ffc757c03441db78e8b145cb6f
Arnold Daniels <arnold@jasny.net>
2015-11-18 23:59:13 -0400

    Don't add pdf.worker.js to pdf.js

--- build.sh
+++ build.sh
@@ -1,11 +1,14 @@
-cp source/build/pdf.worker.js source/web/cmaps/ source/web/images/ source/web/locale/ . -a
-
-cat source/web/l10n.js source/build/pdf.js source/build/pdf.worker.js source/web/compatibility.js source/web/debugger.js source/web/viewer.js > pdf.js
+cat source/web/l10n.js source/build/pdf.js source/web/compatibility.js source/web/debugger.js source/web/viewer.js > pdf.js
 patch pdf.js < pdf.js.patch
+
+cp source/build/pdf.worker.js .
+patch pdf.worker.js < pdf.worker.js.patch
 
 uglifycss source/web/viewer.css > viewer.css
 node grunt/css-prefix.js viewer.css viewer.css pdfjs
 
 sed -r 's/url\((")?images\//url\(\1@pdfjsImagePath\//g' < source/web/viewer.css | uglifycss > viewer.less
 node grunt/css-prefix.js viewer.less viewer.less pdfjs
+
+cp source/web/cmaps/ source/web/images/ source/web/locale/ . -a
 
