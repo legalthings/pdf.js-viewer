acc8da50d53924c0fdcc66c11172e442afbc7373
Arnold Daniels <arnold@jasny.net>
2015-11-19 00:12:23 -0400

    Only warn that translation is missing, if ther is no alternative.

--- pdf.js
+++ pdf.js
@@ -826,8 +826,8 @@ document.webL10n = (function(window, document, undefined) {
   function getL10nData(key, args, fallback) {
     var data = gL10nData[key];
     if (!data) {
-      console.warn('#' + key + ' is undefined.');
       if (!fallback) {
+        console.warn('#' + key + ' is undefined.');
         return null;
       }
       data = fallback;
