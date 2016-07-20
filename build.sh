4a8f2168e328313424daeeba8f18614f5951679f
Arnold Daniels <arnold@jasny.net>
2016-07-20 18:38:49 +0200

    Added upgrading instructions to README Specify source dir as first argument of build script

--- build.sh
+++ build.sh
@@ -1,14 +1,28 @@
-cat source/web/l10n.js source/build/pdf.js source/web/compatibility.js source/web/debugger.js source/web/viewer.js > pdf.js
+#!/bin/bash
+
+if [[ $# < 1 ]]; then
+  echo USAGE $0 SOURCE_PATH 1>&2
+  exit 1
+fi
+
+source="$1"
+
+if [[ ! -f "$source/build/pdf.js" ]]; then
+  echo "$source/build/pdf.js" not found 1>&2
+  exit 1
+fi
+
+cat "$source/web/l10n.js" "$source/build/pdf.js" "$source/web/compatibility.js" "$source/web/debugger.js" "$source/web/viewer.js" > pdf.js
 patch pdf.js < pdf.js.patch
 
-cp source/build/pdf.worker.js .
+cp "$source/build/pdf.worker.js" .
 patch pdf.worker.js < pdf.worker.js.patch
 
-uglifycss source/web/viewer.css > viewer.css
+uglifycss "$source/web/viewer.css" > viewer.css
 node grunt/css-prefix.js viewer.css viewer.css pdfjs
 
-sed -r 's/url\((")?images\//url\(\1@pdfjsImagePath\//g' < source/web/viewer.css | uglifycss > viewer.less
+sed -r 's/url\((")?images\//url\(\1@pdfjsImagePath\//g' < "$source/web/viewer.css" | uglifycss > viewer.less
 node grunt/css-prefix.js viewer.less viewer.less pdfjs
 
-cp source/web/cmaps/ source/web/images/ source/web/locale/ . -a
+cp "$source/web/cmaps/" "$source/web/images/" "$source/web/locale/" . -a
 
